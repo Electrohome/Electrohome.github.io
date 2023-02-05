1cb33f75d5c110db609bb11195dc9827e41a7d54
Electrohome <124122715+Electrohome@users.noreply.github.com>
2023-02-05 15:34:29 +0200

    Add files via upload

---
+++ pages/Java/Laptop-script.js
@@ -0,0 +1,9 @@
+﻿const button = document.querySelector(".type-6 a");
+
+button.addEventListener("touchstart", function () {
+    button.classList.add("active");
+});
+
+button.addEventListener("touchend", function () {
+    button.classList.remove("active");
+});
